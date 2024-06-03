4181ec540d23e3df61b905435a2c9a9d5c20f5bf
msoulard-prolaser <mickael.soulard@pro_laser.fr>
2024-06-03 11:18:29 +0200

    VariablesEnv

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
     #[Route('/hello/{name<(?:\pL|[- ])+>?World}', name: 'app_hello_index')]
-    public function index(string $name): Response
+    public function index(string $name, #[Autowire(param: 'app.sf_version')] string $sfVersion): Response
     {
+        dump($sfVersion);
+
         return $this->render('hello/index.html.twig', [
             'controller_name' => $name,
         ]);
