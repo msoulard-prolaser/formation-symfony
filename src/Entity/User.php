f050c6b4dd4d6eee9869a530b27db7354c2c67c2
msoulard-prolaser <mickael.soulard@pro_laser.fr>
2024-06-06 14:41:46 +0200

    registerUser

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,10 +4,15 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints As Assert;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -27,6 +32,15 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $preferredChannel = null;
 
+    #[Assert\NotBlank([
+        'message' => 'Please enter a password',
+    ])]
+    #[Assert\Length([
+            'min' => 6,
+            'minMessage' => 'Your password should be at least {{ limit }} characters',
+            // max length allowed by Symfony for security reasons
+            'max' => 4096,
+    ])]
     private ?string $plainPassword = null;
     public function getId(): ?int
     {
